b961128dab3f4c3d4d4a78b7ba458b726f8dd14a
Alejandro Mostajo <amostajo@amsgames.com>
2015-11-03 09:20:32 -0600

    Various changes.

--- config/plugin.php
+++ config/plugin.php
@@ -18,4 +18,28 @@ return [
 
 	'addons' => [],
 
+	'cache' => [
+
+		// Enables or disables cache
+		'enabled' 		=> true,
+		// files, sqlite, auto (files), apc, wincache, xcache, memcache, memcached
+		'storage'		=> 'auto',
+		// Default path for files
+		'path'			=> __DIR__ . '/../cache/',
+		// It will create a path by PATH/securityKey
+		'securityKey'	=> '',
+		// FallBack Driver
+		'fallback'		=> [
+							'memcache'	=>	'files',
+							'apc'		=>	'sqlite',
+						],
+		// .htaccess protect
+		'htaccess'		=> true,
+		// Default Memcache Server
+		'server'		=> [
+							[ '127.0.0.1', 11211, 1 ],
+						],
+
+	],
+
 ];
